4e8e2a4b9ec7c06fb85a70d9b2fff9223917a0ae
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-06-20 01:11:43 +0700

    netcore

---
+++ codes/api/api.Server/Migrations/20240619173334_First.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookPrice = table.Column<double>(type: "float", nullable: false),
+                    BookQuantity = table.Column<int>(type: "int", nullable: false),
+                    BookCover = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book",
+                columns: new[] { "BookId", "BookCover", "BookPrice", "BookQuantity", "BookTitle" },
+                values: new object[,]
+                {
+                    { 1, "https://m.media-amazon.com/images/I/61svWgrmT0L._AC_UF1000,1000_QL80_.jpg", 25.0, 10, "The Full Stack Developer" },
+                    { 2, "https://m.media-amazon.com/images/I/81iua0Wh34L._AC_UF1000,1000_QL80_.jpg", 35.0, 20, "ASP.NET Core Application Development" },
+                    { 3, "https://media.springernature.com/full/springer-static/cover-hires/book/978-1-4842-3781-6", 45.0, 30, "Getting to know Vue.js" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
--- codes/api/api.Server/obj/Debug/net8.0/api.Server.AssemblyInfo.cs
+++ codes/api/api.Server/obj/Debug/net8.0/api.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f774e6e02740219e8f656dc116d22c354bc957e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7beece67dfb1ea80a8951b04f9c3ae80c038d057")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
